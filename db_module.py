f8dd6d475a6249bf16476adaec9a710ada5dd28e
PeterB789 <peter.bischofberger@gmx.de>
2022-11-30 20:34:31 +0100

    poster

--- db_module.py
+++ db_module.py
@@ -49,11 +49,13 @@ def db_get_username(id):
     username = my_cursor.fetchall()
     return username[0][0]
 
+
 def db_get_user_id(rfid):
     sql = f"SELECT user_id FROM users WHERE rfid = {rfid};"
     my_cursor.execute(sql)
     user_id = my_cursor.fetchall()
     return user_id[0][0]
+
 
 def db_delete_user(id):
     if db_userid_check(id):
